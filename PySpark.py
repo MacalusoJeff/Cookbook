243192603b56b1e99646e89b72e11cb5ce20984e
Jeff Macaluso <133994400+MacalusoJeff@users.noreply.github.com>
2023-06-22 14:47:12 -0700

    Single quotes for consistency

--- PySpark.py
+++ PySpark.py
@@ -7,12 +7,12 @@ df.select([F.count(F.when(F.isnan(c), c)).alias(c) for c in df.columns]).show()
 df.fillna(0, subset=['a', 'b'])
 
 # Converting all boolean columns to integers
-df = df.select([df[column].cast("integer").alias(column) if column_type == "boolean" else df[column] for column, column_type in df.dtypes])
+df = df.select([df[column].cast('integer').alias(column) if column_type == 'boolean' else df[column] for column, column_type in df.dtypes])
 
 # String indexing multiple columns
 from pyspark.ml.feature import StringIndexer
 categorical_cols = ['col_1', 'col_2']
-indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(df) for column in categorical_cols]
+indexers = [StringIndexer(inputCol=column, outputCol=column+'_index').fit(df) for column in categorical_cols]
 pipeline = Pipeline(stages=indexers)
 df = pipeline.fit(df).transform(df)
 
@@ -20,7 +20,7 @@ df = pipeline.fit(df).transform(df)
 from pyspark.ml.feature import VectorAssembler
 cols_to_drop = ['col_1', 'col_2']
 feature_cols = [feature for feature in df.columns if feature not in cols_to_drop]
-featurizer = VectorAssembler(inputCols=feature_cols, outputCol="features")
+featurizer = VectorAssembler(inputCols=feature_cols, outputCol='features')
 train, test = df.randomSplit([0.8, 0.2], seed=46)
 train_data = featurizer.transform(train)['label', 'features']
 test_data = featurizer.transform(test)['label', 'features']
@@ -29,8 +29,8 @@ test_data = featurizer.transform(test)['label', 'features']
 from synapse.ml.train import ComputeModelStatistics
 predictions = model.transform(test_data)
 metrics = ComputeModelStatistics(
-    evaluationMetric="classification",
-    labelCol="label",
-    scoredLabelsCol="prediction",
+    evaluationMetric='classification',
+    labelCol='label',
+    scoredLabelsCol='prediction',
 ).transform(predictions)
 display(metrics)
