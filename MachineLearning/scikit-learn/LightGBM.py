d439d7b04918ca668ca95fb46aa874bec0e28878
Jeff Macaluso <133994400+MacalusoJeff@users.noreply.github.com>
2023-07-07 14:50:55 -0700

    Updated early stopping

--- MachineLearning/scikit-learn/LightGBM.py
+++ MachineLearning/scikit-learn/LightGBM.py
@@ -6,30 +6,29 @@ from sklearn.metrics import mean_squared_error
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=46)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=46)
 
-# Early stopping w/ sklearn API
+### Early stopping w/ sklearn API
 # Set the hyperparameters for LightGBM
 # params documentation: https://lightgbm.readthedocs.io/en/latest/Parameters.html
 params = {
     'metric': 'mse',
-    'num_leaves': 31,
     'learning_rate': 0.01,
-    'num_boost_round': 10000,  # Early stopping should catch this earlier and keep it from overfitting
-    'early_stopping_rounds': 10,
-    'feature_fraction': 0.9,
-    'verbose': 10
+    'n_estimators': 30000,  # Early stopping should catch this earlier and keep it from overfitting
+    'num_leaves': 31,
+    'feature_fraction': 0.9
 }
 
 # Train the model with early stopping
-model = lgb.LGBMRegressor(**params)
-print('Starting training')
-model.fit(X_train, y_train, eval_set=[(X_val, y_val)], n_jobs=-1)
-print('Done at {0} iterations'.format(model.booster_.best_iteration))
+model = lgb.LGBMRegressor(n_jobs=-1, random_state=46, **params)
+model.fit(X_train, y_train,
+          eval_set=[(X_val, y_val)],
+          callbacks=[lgb.early_stopping(100)])
+print(f'Done at {model.best_iteration_} iterations')
 
 # Predict on the test set
-y_pred = model.predict(X_test, num_iteration=model.booster_.best_iteration)
+y_pred = model.predict(X_test, num_iteration=model.best_iteration_)
 
 
-# Early stopping with the LGBM API
+### Early stopping with the LGBM API
 train_data = lgb.Dataset(X_train, label=y_train)
 val_data = lgb.Dataset(X_val, label=y_val)
 
